59618943afa13a9d6b8e35ea62f7fe8ac3bd90ff
Bruno Alves <vespidhook@gmail.com>
2022-07-27 01:25:58 -0300

    auth stack

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { StateProvider } from './src/contexts/StateContext';
 
 export default () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <StateProvider>
+      <View>
+        <Text>App</Text>
+      </View>
+    </StateProvider>
   );
 }
