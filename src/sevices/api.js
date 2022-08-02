e2393384a9efd7127485eef9c341534ab8e48a8f
Bruno Alves <vespidhook@gmail.com>
2022-08-02 07:52:17 -0300

    tela de login

--- src/sevices/api.js
+++ src/sevices/api.js
@@ -10,10 +10,27 @@ const request = async (method, endpoint, params, token = null) => {
     switch(method) {
         case "get":
             let queryString = new URLSearchParams(params).toString();
-            fullUrl += `?${queryString}`;
-            
+            fullUrl += `?${queryString}`;            
+        break;
+        case 'post':
+        case 'put':
+        case 'delete':
+            body = JSON.stringify(params);
         break;
     }
+
+    let hederes = {'Content-Type': 'application/json'};
+    if(token) {
+        hederes.Authorization = `Bearer ${token}`;
+    }
+
+    let req = await fetch(fullUrl, {
+        method,
+        hederes,
+        body
+    });
+    let json = await req.json();
+    return json;
 }
 
 export default {
@@ -22,5 +39,7 @@ export default {
     },
     validateToken: async () => {
         let token = await AsyncStorage.getItem("token");
+        let json = await request("post", "auth/validate", {}, token);
+        return json;
     }
 }
