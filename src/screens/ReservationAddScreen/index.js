782e7974d81f12ae73dd6c963aa7fa535d4e17cd
Bruno Alves <vespidhook@gmail.com>
2022-09-09 00:05:45 -0300

    calendar picker

--- src/screens/ReservationAddScreen/index.js
+++ src/screens/ReservationAddScreen/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import CalendarPicker from 'react-native-calendar-picker';
 import C from './style';
 
 import {useStateValue} from '../../contexts/StateContext';
@@ -10,7 +11,7 @@ export default () => {
   const route = useRoute();
   const [context, dispatch] = useStateValue();
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(()=>{
     const unsubscribe = navigation.addListener('focus', () => {
@@ -21,6 +22,14 @@ export default () => {
     return unsubscribe;    
   }, [navigation, route]);
 
+  const minDate = new Date();
+  const maxDate = new Date();
+  maxDate.setMonth(maxDate.getMonth() + 3);
+
+  handleDateChange = () => {
+
+  }
+
   return (
     <C.Container>
       <C.Scroller contentContainerStyle={{paddingBottom: 40}}>
@@ -28,6 +37,38 @@ export default () => {
 
         {loading &&
           <C.LoadingIcon size="large" color="#8863e6" />
+        }
+
+        {!loading &&
+          <C.CalendarArea>
+            <CalendarPicker 
+              onDateChange={handleDateChange}
+              minDate={minDate}
+              maxDate={maxDate}
+              weekdays={['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']}
+              months={[ 'Janeiro',
+                        'Fevereiro',
+                        'Março',
+                        'Abril',
+                        'Maio',
+                        'Junho',
+                        'Julho',
+                        'Agosto',
+                        'Setembro',
+                        'Outubro',
+                        'Novembro',
+                        'Dezembro'
+                      ]}
+              previousTitle="Anterior"
+              nextTitle="Proximo"
+              selectMonthTitle="Selecione o mês em "
+              selectYearTitle="Selecione o ano"
+              selectedDayColor="#8863e6"
+              selectedDayTextColor="#FFFFFF"
+              todayBackgroundColor="transparent"
+              todayTextStyle="#000000"
+            />
+          </C.CalendarArea>
         }
       </C.Scroller>
     </C.Container>
--- src/screens/ReservationAddScreen/style.js
+++ src/screens/ReservationAddScreen/style.js
@@ -4,7 +4,6 @@ export default {
     Container: styled.SafeAreaView`
         flex: 1;
         background-color: #f5f6fa;
-        padding: 20px;
     `,    
     Scroller: styled.ScrollView`
         flex: 1;
@@ -14,5 +13,8 @@ export default {
     `,
     LoadingIcon: styled.ActivityIndicator`
         margin-top: 20px;
+    `,
+    CalendarArea: styled.View`
+        margin: 20px;
     `,
 }
